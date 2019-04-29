6901ad7ca11eb587a91d779ac00a2a5006d0f8f7
Taku Kobachi <>
2019-04-29 21:28:40 +0900

    adjustment

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const HDWallet = require('truffle-hdwallet-provider');
+ const fs = require('fs');
+ const infuraKey = fs.readFileSync(".infurakey").toString().trim();
+ const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
 
 module.exports = {
   /**
@@ -68,6 +67,13 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+        provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: "*",
+        gas: 5500000,
+    },
+
 
     // Useful for private networks
     // private: {
